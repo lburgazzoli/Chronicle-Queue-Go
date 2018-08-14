ba921a4791f28c422dc7aecc806fc492a9fe1fd7
lburgazzoli <lburgazzoli@gmail.com>
2018-08-14 12:11:27 +0200

    typo

--- bytes/utils.go
+++ bytes/utils.go
@@ -5,6 +5,6 @@ import "log"
 // CheckBounds --
 func CheckBounds(index int32, length int32, limit int32) {
 	if (index + length) > limit {
-		log.Fatalf("ou of ounds. index: %d, length: %d, capacity: %d", index, length, limit)
+		log.Fatalf("ou of bounds. index: %d, length: %d, capacity: %d", index, length, limit)
 	}
 }
